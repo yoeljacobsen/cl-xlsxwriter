77625e34e21ed2d5c9906d0876ae0b6b5f6206cb
Yoel Jacobsen <yoel.jacobsen@gmail.com>
2019-09-20 14:44:37 +0300

    More formatting

--- xlsxwriter.lisp
+++ xlsxwriter.lisp
@@ -139,6 +139,12 @@
   (double :double)
   (format lxw-format))
 
+(defcfun "worksheet_set_column" lxw-error
+  (first-col lxw-col)
+  (last-col lxw-col)
+  (width :double)
+  (format lxw-format))
+
 (defcfun "workbook_close" lxw-error
   (workbook lxw-workbook))
 
@@ -190,6 +196,9 @@
   (format lxw-format)
   (border lxw-format-borders))
 
+(defcfun "format_set_text_wrap" :void
+  (format lxw-format))
+
 ;;; Helper functions
 
 ;;; Memorize current row and col. If only a new is provided, col is reset to 0.
@@ -200,11 +209,14 @@
 
 (let ((current-row 0)
       (current-col 0))
-  (defun worksheet-write (worksheet value &key row col format)
+  (defun worksheet-write (worksheet value &key row col format new-row)
     "Add a value to the worksheet. If row/col are not provided, add to the left"
-    (let* ((actual-row (if row row current-row))
+    (let* ((actual-row (if row row
+                           (if new-row (1+ current-row)current-row)))
            (actual-col (if col col
-                          (if (= actual-row current-row) current-col 0)))
+                           (if (and (null new-row)
+                                    (= actual-row current-row))
+                               current-col 0)))
            (actual-format (if format format (null-pointer))))
       (cond ((stringp value)
              (worksheet-write-string worksheet actual-row actual-col value actual-format))
@@ -229,17 +241,20 @@
 (defun test-helper (filename)
   (let* ((workbook (workbook-new filename))
          (worksheet (workbook-add-worksheet workbook "sheet1"))
+         (items `(1 "two" 3.0 "four" 5/6 "six" "ML without any newline characters" ,(format nil "This~%is~%a multi-line~%Cell")))
          (f1 (workbook-add-format workbook))
          (f2 (workbook-add-format workbook)))
     (format-set-bold f1)
+    (format-set-text-wrap f1)
+    (format-set-align f1 :lxw-align-vertical-justify)
     (format-set-font-color f2 lxw-color-white)
     (format-set-bg-color f2 lxw-color-blue)
     (format-set-pattern f2 :lxw-pattern-solid)
     (format-set-align f2 :lxw-align-center)
     (format-set-border f2 :lxw-border-thick)
-    (loop for val in '(1 "two" 3.0 "four" 5/6 "six")
-          do (worksheet-write worksheet val :format f1))
-    (loop for val in '(1 "two" 3.0 "four" 5/6 "six")
+    (loop for val in items
+          do (worksheet-write worksheet val :row 0 :format f1))
+    (loop for val in items
           do (worksheet-write worksheet val :row 3 :format f2))
     (workbook-close workbook)))
 
